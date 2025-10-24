893610e4fa1f6ff6f17cb37155664472962cd497
mahmoudayachi <91746801+mahmoudayachi@users.noreply.github.com>
2025-10-24 13:22:58 +0100

    Update ZoomMeetingInfo.java

--- Mentorship-app/src/main/java/com/example/Mentorship_app/Services/jwt/ZoomMeetingInfo.java
+++ Mentorship-app/src/main/java/com/example/Mentorship_app/Services/jwt/ZoomMeetingInfo.java
@@ -2,10 +2,10 @@ package com.example.Mentorship_app.Services.jwt;
 
 public class ZoomMeetingInfo {
     private String meetingId;
-    private String joinUrl;   // for participants (mentees)
-    private String startUrl;  // for host (mentor)
+    private String joinUrl;   
+    private String startUrl;  
 
-    // constructors
+   
     public ZoomMeetingInfo() {}
 
     public ZoomMeetingInfo(String meetingId, String joinUrl, String startUrl) {
@@ -14,7 +14,7 @@ public class ZoomMeetingInfo {
         this.startUrl = startUrl;
     }
 
-    // getters and setters
+
     public String getMeetingId() { return meetingId; }
     public void setMeetingId(String meetingId) { this.meetingId = meetingId; }
 
